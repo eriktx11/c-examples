0b02cb3be6aa8ffb55e47f1f75b2847e7c462e94
eriktx11 <erik@llerenapiano.com>
2019-07-28 20:21:24 -0500

    Example 3

---
+++ app/src/main/java/interview.java
@@ -0,0 +1,12 @@
+class interview{
+    public static void main (String[] args){
+        int bucky[] = new int[3];
+
+        bucky [0]= 323;
+        bucky [1]= 33;
+        bucky [2]= 123;
+
+        System.out.println(bucky[2]);
+    }
+
+}
